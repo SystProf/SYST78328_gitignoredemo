4c87abdaa078cfb0821abcaf197d53af53b4325f
megha <megha.patel2@sheridancollege.ca>
2022-01-26 13:32:46 -0500

    first commit for shape project

---
+++ src/shape/Square.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package shape;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class Square {
+    private int sides;
+
+    /**
+     * @return the sides
+     */
+    public int getSides() {
+        return sides;
+    }
+
+    /**
+     * @param sides the sides to set
+     */
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+}
